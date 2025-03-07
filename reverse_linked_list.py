95685835702c8bac7d2ef8893517f666e66ac553
andervies <anderclace@gmail.com>
2025-03-07 22:07:07 +0100

    Add reverse list solution

---
+++ reverse_linked_list.py
@@ -0,0 +1,18 @@
+# Definition for singly-linked list.
+from typing import Optional
+
+
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+class Solution:
+    def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        prev = None
+        while head:
+            nexxt = head.next
+            head.next = prev
+            prev = head
+            head = nexxt
+
+        return prev

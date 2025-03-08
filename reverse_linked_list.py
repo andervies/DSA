9e888813fc3954991d6a9ccd3ab04431ac9ea7ce
andervies <anderclace@gmail.com>
2025-03-08 21:57:45 +0100

    Update reverse_linked_list.py

--- reverse_linked_list.py
+++ reverse_linked_list.py
@@ -16,3 +16,26 @@ class Solution:
             head = nexxt
 
         return prev
+
+
+
+linked_list = None
+for i in range(1,6):
+    linked_list = ListNode(i, linked_list)
+
+
+def print_linked_list_vals(linked_list):
+    while linked_list:
+        print(linked_list.val)
+        linked_list = linked_list.next
+
+print_linked_list_vals(linked_list)
+
+
+## Let's test our algorithm
+soln = Solution()
+print("Reversing the linked list above")
+print_linked_list_vals(soln.reverseList(linked_list))
+
+
+

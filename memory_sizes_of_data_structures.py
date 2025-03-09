24a23041c5adbded09ef3204688a5366b60b476e
andervies <anderclace@gmail.com>
2025-03-09 20:53:20 +0100

    Add array to the data structures

--- memory_sizes_of_data_structures.py
+++ memory_sizes_of_data_structures.py
@@ -1,9 +1,11 @@
 import sys
 
-# Create a tuple and a set with the same elements
+# Create different data structures with the same elements
 tuple_example = ("(", "[", "{")
 set_example = {"(", "[", "{"}
+array_example = ["(", "[", "{"]
 
 # Check their memory usage
 print("Memory size of tuple:", sys.getsizeof(tuple_example))
 print("Memory size of set:", sys.getsizeof(set_example))
+print("Memory size of array/list:", sys.getsizeof(array_example))

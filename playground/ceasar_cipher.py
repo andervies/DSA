d5264a5958662e7e82368cc5261d3a1c18df6ea4
andervies <anderclace@gmail.com>
2025-03-12 20:11:25 +0100

    Create caesar class and add cipher and decipher methods

--- playground/ceasar_cipher.py
+++ playground/ceasar_cipher.py
@@ -13,4 +13,42 @@ def decipher_ceasar(key, cipher_text):
 
     return plaintext
 
-print(decipher_ceasar(7, "khhspkhylfvbavjylhalhjhlzhyjpwolykljyfwapvuhsnvypaotbzpunhnpclurlf"))
+
+class Caesar:
+
+    def cipher(self, plain_text, key):
+        cipher_text = ""
+
+        for char in plain_text.encode("ascii"):
+            if char >= 65 and char < 91:
+                numm =(char - 65 + key) % 26
+                cipher_text += chr(numm+65)
+            elif char >= 97 and char < 123:
+                numm = (char - 97 + key) % 26
+                cipher_text += chr(numm + 97)
+            else:
+                cipher_text += chr(char)
+        return cipher_text
+
+    def decipher(self, cipher_text, key):
+        plain_text = ""
+
+        for char in cipher_text.encode("ascii"):
+            if char >= 65 and char < 91:
+                numm = (char - 65 - key) % 26
+                print(numm)
+                plain_text += chr(numm + 65)
+            elif char >= 97 and char < 123:
+                numm = (char - 97 - key) % 26
+                plain_text += chr(numm + 97)
+            else:
+                plain_text += chr(char)
+        return plain_text
+
+
+caesar = Caesar()
+cipher_text = caesar.cipher("Welcome to the internet! have a look around", 4)
+plain_text = caesar.decipher(cipher_text, 4)
+
+print(cipher_text)
+print(plain_text)

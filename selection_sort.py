5c2cde4b71b0a8fb01680d79677c110a06d25397
andervies <anderclace@gmail.com>
2025-03-22 12:25:27 +0100

    Add selection sort algorithm

---
+++ selection_sort.py
@@ -0,0 +1,22 @@
+def find_smallest_index(arr):
+    smallest = arr[0]
+    smallest_index = 0
+    for i in range(1, len(arr)):
+        if arr[i] < smallest:
+            smallest = arr[i]
+            smallest_index = i
+    return smallest_index
+
+# print(find_smallest_index([9,3,1,5,3]))
+
+def selection_sort(arr):
+    sorted_list = []
+
+    for i in range(len(arr)):
+        print(i)
+        smallest_index = find_smallest_index(arr)
+        sorted_list.append(arr.pop(smallest_index))
+
+    return sorted_list
+
+print(selection_sort([5, 3, 6, 2, 10]))

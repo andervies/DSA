4e7c5df570ede47caf2bdcbaa44d21bff2a1fae3
andervies <anderclace@gmail.com>
2025-03-28 14:20:28 +0100

    Add addAtIndex, deleteAtIndex, and print methods

--- design_linked_list.py
+++ design_linked_list.py
@@ -14,17 +14,15 @@ class MyLinkedList:
         # self.tail.prev = self.head
 
     def get(self, index: int) -> int:
-        index_node = self.head.next
-        count = -1
+        curr = self.head.next
+        count = 0
 
-        while index_node:
-            count += 1
-
+        while curr :
             if count == index:
-                print(count)
-                return index_node.val
-            elif index_node:
-                index_node = index_node.next
+                return curr.val
+            elif curr:
+                curr = curr.next
+                count += 1
 
         return -1
 
@@ -41,30 +39,77 @@ class MyLinkedList:
         self.tail = self.tail.next
 
     def addAtIndex(self, index: int, val: int) -> None:
+        count = 0
+        # Let curr trail behind the count since for a singly linked list we'd need the previous node before the indexth
+        curr = self.head
 
-        return True
+        while curr and count < index:
+            count += 1
+            curr = curr.next
+
+
+        if curr:
+            new_node = ListNode(val)
+            new_node.next = curr.next
+            curr.next = new_node
+
+            if new_node.next is None:
+                self.tail = new_node
+
+
 
     def deleteAtIndex(self, index: int) -> None:
+        count = 0
+        # Let curr trail behind the count since for a singly linked list we'd need the previous node before the indexth
+        curr = self.head
 
-        return True
+        while curr and count < index:
+            count += 1
+            curr = curr.next
 
-# Your MyLinkedList object will be instantiated and called as such:
-# obj = MyLinkedList()
-# param_1 = obj.get(index)
-# obj.addAtHead(val)
-# obj.addAtTail(val)
-# obj.addAtIndex(index,val)
-# obj.deleteAtIndex(index)
+        if curr and curr != self.tail:
+            curr.next = curr.next.next
+
+            if curr.next is None:
+                self.tail = curr
+            # if curr == self.tail:
+
+
+
+    def print(self):
+        curr = self.head.next
+        while curr:
+            print(' -> ', curr.val)
+            curr = curr.next
+        print()
+
+
 
 my_linked = MyLinkedList()
+my_linked.addAtHead(7)
 my_linked.addAtHead(2)
-my_linked.addAtTail(3)
+my_linked.addAtHead(1)
+my_linked.addAtIndex(3,0)
+my_linked.print()
+my_linked.deleteAtIndex(2)
+my_linked.print()
+my_linked.addAtHead(6)
+my_linked.print()
 my_linked.addAtTail(4)
+my_linked.print()
 
-curr = my_linked.head
-# for i in range(4):
-#
-#     print(curr.val)
-#     curr = curr.next
+print(f"getting index 4 {my_linked.get(4)}")
 
-print(my_linked.get(5))
+my_linked.addAtHead(4)
+my_linked.print()
+my_linked.addAtIndex(5,0)
+my_linked.print()
+my_linked.addAtHead(6)
+my_linked.print()
+
+
+print(my_linked.tail.val)
+my_linked.deleteAtIndex(0)
+print(my_linked.tail.val)
+
+# ToDo: Assert that the tail pointer stays relatively consistent across all operations
